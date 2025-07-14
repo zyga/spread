11a0ecea3795309d18d92da4c43339717b5de851
Zygmunt Krynicki <me@zygoon.pl>
2025-07-14 09:20:15 +0200

    unspread: add support for project and suite artifacts
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- spread/project.go
+++ spread/project.go
@@ -22,6 +22,8 @@ type Project struct {
 
 	Environment *Environment
 
+	Artifacts []string
+
 	Repack      string
 	Reroot      string `yaml:"reroot"`
 	Prepare     string
@@ -317,6 +319,8 @@ type Suite struct {
 	Systems  []string
 	Backends []string
 
+	Artifacts []string
+
 	Variants    []string
 	Environment *Environment
 
@@ -463,6 +467,10 @@ func join(scripts ...string) string {
 	return buf.String()
 }
 
+func isPathRelativeAndInsideWorkingDir(path string) bool {
+	return !filepath.IsAbs(path) && path == filepath.Clean(path) && !strings.HasPrefix(path, "../")
+}
+
 type jobsByName []*Job
 
 func (jobs jobsByName) Len() int      { return len(jobs) }
@@ -520,6 +528,12 @@ func Load(path string) (*Project, error) {
 
 	if err := checkEnv(project, &project.Environment); err != nil {
 		return nil, err
+	}
+
+	for _, fname := range project.Artifacts {
+		if !isPathRelativeAndInsideWorkingDir(fname) {
+			return nil, fmt.Errorf("the project has an invalid artifact path: %s", fname)
+		}
 	}
 
 	for bname, backend := range project.Backends {
@@ -622,6 +636,12 @@ func Load(path string) (*Project, error) {
 			return nil, fmt.Errorf("%s is missing a summary", suite)
 		}
 
+		for _, fname := range suite.Artifacts {
+			if !isPathRelativeAndInsideWorkingDir(fname) {
+				return nil, fmt.Errorf("the suite has an invalid artifact path: %s", fname)
+			}
+		}
+
 		if err := checkEnv(suite, &suite.Environment); err != nil {
 			return nil, err
 		}
@@ -685,7 +705,7 @@ func Load(path string) (*Project, error) {
 			}
 
 			for _, fname := range task.Artifacts {
-				if filepath.IsAbs(fname) || fname != filepath.Clean(fname) || strings.HasPrefix(fname, "../") {
+				if !isPathRelativeAndInsideWorkingDir(fname) {
 					return nil, fmt.Errorf("%s has improper artifact path: %s", task.Name, fname)
 				}
 			}
